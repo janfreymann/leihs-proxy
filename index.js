2e58101463c1c4aac7d94b54d4d06dcb287eb9a9
Jan Freymann <jan.freymann@klangerfinder.de>
2017-01-08 16:09:41 +0100

    hand over package logic

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ var connection = mysql.createConnection({
 
 var selects = [];
 var simpleselect = {};
+var selects2 = [];
+var simpleselect_handover = {};
 
 var customscript = ` 
 <!-- Modal -->
@@ -87,6 +89,93 @@ function checkPackageCodes() {
 </script>
 `;
 
+var handoverscript  = ` 
+<script>
+
+var packageCodes = [];
+var replacedHtml = '';
+
+$(document).ready(function(){
+  console.log("hand over proxy script at work.");
+  $('.button.green').click(function() {
+    setTimeout(function() {
+    $('.button.green').hide();
+    replacedHtml = $('.padding-bottom-m.margin-bottom-m.no-last-child-margin').html();
+   // $('.row.margin-top-s.padding-horizontal-l').append();
+   $('.padding-bottom-m.margin-bottom-m.no-last-child-margin').html('<form id="packageForm"><ul id="mht_package_list"></ul><button type="button" style="margin-top:20px" onClick="checkPackageCodes();">Check & Close</button></form>');
+    $("input[id^=assigned-item-]").each(function(i, obj) {
+      var item_id = $(obj).val();
+      
+      if(item_id.length > 0) {
+          $.get( "/mht_get_package_children?id=" + item_id, function( data ) {
+        console.log("Result for item " + item_id + ": " + JSON.stringify(data));
+
+        // example: [{"inventory_code":"2","model_id":2,"product":"ModelB"},{"inventory_code":"1","model_id":1,"product":"ModelA"}] hand_over:741:9
+
+        // row margin-top-s padding-horizontal-l
+        
+        for(var k in data) {
+          $('#mht_package_list').append('<li id="li' + data[k].inventory_code + '">' + data[k].product + '<input type="text" id="ic' + data[k].inventory_code + '"></input></li>');        
+          packageCodes.push(data[k].inventory_code);
+        }
+       });
+      }   
+    });
+
+    // div aria-hidden="false" class="modal fade ui-modal medium in ui-shown" 
+
+    $("div.modal.fade.ui-modal.medium.in.ui-shown").on('hidden.bs.modal', function (e) {
+       closedModal();
+      });  
+      $("div.modal-close").click(function() {
+        closedModal();
+      });
+    }, 1000);
+  });
+  $("input[id^=assigned-item-]").each(function(i, obj) {
+    console.log("found item: " + obj + " value: " + $(obj).val());
+  });
+});
+
+function restoreHtml() {
+   $('.padding-bottom-m.margin-bottom-m.no-last-child-margin').html(replacedHtml);
+}
+function closedModal() {
+ console.log("closed modal!");
+ packageCodes = []; //reset 
+ restoreHtml();
+ $('.button.green').show();
+}
+
+function checkPackageCodes() {
+  if(packageCodes.length == 0) {
+    restoreHtml();
+    $('.button.green').show();
+  }
+  else {
+    var canClose = true;
+    for(var i in packageCodes) {
+      var k = packageCodes[i];
+      if($('#ic' + k).val() == k) {
+        $('#ic' + k).hide();
+        $('#li' + k).append('<i class="fa fa-check margin-right-m"></i>')
+      }
+      else {
+        console.log("ic" + k + " does not match with " + k);
+        $('#ic' + k).val('?');
+        canClose = false;
+      }
+    }
+    if(canClose) {
+      restoreHtml();
+      $('.button.green').show();
+    }  
+  }
+}
+
+</script>
+`;
+
 // col2of10 line-col text-align-center
 
 
@@ -106,14 +195,42 @@ simpleselect.func = function (node) {
     
 }
 
+simpleselect_handover.query = 'body';
+simpleselect_handover.func = function(node) {
+    var rs = node.createReadStream();
+  var ws = node.createWriteStream({outer: false});
+
+  rs.pipe(ws, {end: false});
+
+  
+  
+  // When the read stream has ended, attach our style to the end
+  rs.on('end', function(){
+    ws.end(handoverscript);
+  });
+    
+}
+
 selects.push(simpleselect);
+selects2.push(simpleselect_handover);
 
 var only = function(middleware) {
     return function(req, res, next) {
       var tmp = req.path.split('/');
       var endswith = tmp.pop();
-      console.log("endswith: " + endswith + " method: " + req.method);
       if((endswith == 'take_back') && (req.method == 'GET')) {
+        return middleware(req, res, next);
+      }
+      else {
+        return next();
+      }
+    };
+};
+var only2 = function(middleware) {
+    return function(req, res, next) {
+      var tmp = req.path.split('/');
+      var endswith = tmp.pop();
+      if((endswith == 'hand_over') && (req.method == 'GET')) {
         return middleware(req, res, next);
       }
       else {
@@ -123,8 +240,10 @@ var only = function(middleware) {
 };
 
 var harmon = require('harmon-binary')([], selects);
+var harmon2 = require('harmon-binary')([], selects2);
 
-app.use(only(harmon));
+app.use(only(harmon)); //take back
+app.use(only2(harmon2)); //hand over
 
 app.get('/mht_get_package_children', function(req, res) {
   console.log('processing /mht_get_package_children');
